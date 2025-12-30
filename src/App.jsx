aa4e4bfb93c081a5fbec384bc92f459e73fca2a9
KimDuHyun-web <duhyeon467@gmail.com>
2025-12-30 17:34:51 +0900

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import Header from './components/Header'
+
+function App() {
+  return (
+    <>
+      <Header/>
+      <Home/>
+      <Layout/>
+    </>
+  )
+}
+
+export default App
