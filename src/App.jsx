d2eb2919a20f612e865cba394dc2d3ea5b5bb9c9
KimDuHyun-web <duhyeon467@gmail.com>
2025-12-31 17:08:26 +0900

    again

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,8 @@
-import { Route, Routes } from 'react-router'
-import { useEffect, useState } from 'react'
 import './App.css'
-import Header from './components/Header'
-import Home from './pages/home'
-import Post from './pages/post'
-import Posts from './pages/posts'
 
 function App() {
-  const [list, setList] = useState([]);
-
-  useEffect(()=>{
-    fetch('/data/blog.json')
-    .then(res=>res.json())
-    .then(data=>setList(data))
-  }, [])
-
   return (
     <>
-      <Header/>
-      <Routes>
-        <Route path="/" element={<Home data={list}/>}/>
-        <Route path="/posts" element={<Posts data={list}/>}/>
-        <Route path="/posts/:id" element={<Post data={list}/>}/>
-      </Routes>
     </>
   )
 }
--- src/components/Header.jsx
+++
@@ -1,6 +0,0 @@
-const Header = ()=>{
-  return (
-    <h1>Ajax-Router-Mission</h1>
-  )
-}
-export default Header;
